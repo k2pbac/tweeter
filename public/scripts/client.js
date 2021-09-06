d5d2cd8d62942ecc0114edcc618e36c80cf8644b
k2pbac <krisbachan123@hotmail.com>
2021-09-06 19:50:03 -0400

    add particles to header

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,4 +4,10 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function () {});
+$(document).ready(function () {
+  tsParticles.loadJSON(
+    "tsparticles",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/199011/XjRprX_particles.json?",
+    function () {}
+  );
+});
