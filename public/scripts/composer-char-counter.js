85bc7b365c48d9f0396bf7a7bdd51f8e816babb6
k2pbac <krisbachan123@hotmail.com>
2021-09-06 22:21:05 -0400

    add on input for select to update count of letters in textarea

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,21 @@
 $(document).ready(function () {
-  // --- our code goes here ---
+  $("#tweet-text").on("input", function (event) {
+    let length = $(this).val().length;
+
+    if (140 - length < 0) {
+      let sliced = $(this).val().slice(0, 140);
+      $(this).val($.trim(sliced));
+      $(this).siblings("div").find("output").html(0);
+      $(this).addClass("invalid-input");
+      $(this).siblings(".error").show();
+      event.preventDefault();
+    } else {
+      $(this).siblings(".error").hide();
+      $(this).removeClass("invalid-input");
+      $(this)
+        .siblings("div")
+        .find("output")
+        .html(140 - length);
+    }
+  });
 });
