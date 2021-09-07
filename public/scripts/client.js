70b85c8dc77b9361d9b8b479db7789d57f28a5de
k2pbac <krisbachan123@hotmail.com>
2021-09-07 14:30:39 -0400

    implement functionality to send a get and post request to /tweets with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,35 +1,3 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
-
 $(document).ready(function () {
   tsParticles.loadJSON(
     "tsparticles",
@@ -37,7 +5,34 @@ $(document).ready(function () {
     function () {}
   );
 
-  renderTweets(data);
+  $("main.container form").on("submit", function (e) {
+    e.preventDefault();
+    const data = $(this).serialize();
+    if (
+      data.slice(5).length &&
+      data.slice(5) !== null &&
+      data.slice(5).length <= 140
+    ) {
+      $.ajax({
+        method: "POST",
+        url: "/tweets",
+        data,
+      }).done(function (response) {
+        console.log("success");
+      });
+    }
+  });
+
+  const loadTweets = () => {
+    $.ajax({
+      method: "GET",
+      url: "/tweets",
+    }).done(function (response) {
+      renderTweets(response);
+    });
+  };
+
+  loadTweets();
 });
 
 const createTweetElement = (tweet) => {
